331abb6fdc7fbb027aac7c4d23fdda50221fceec
tedzzh <ted.zhangxiao@gmail.com>
2024-09-12 19:44:36 +0800

    Update intlMiddleware.ts

--- middlewares/intlMiddleware.ts
+++ middlewares/intlMiddleware.ts
@@ -4,6 +4,8 @@ import { localePrefix } from '@/app/navigation';
 
 import { locales } from '../i18n';
 
+const localePrefix = 'as-needed';
+
 const intlMiddleware = createMiddleware({
   locales,
   defaultLocale: 'en',
