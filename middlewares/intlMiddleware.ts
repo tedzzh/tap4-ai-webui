c8caa44e22de52381b3ed47a4710d475347e88b4
tedzzh <ted.zhangxiao@gmail.com>
2024-09-12 19:53:52 +0800

    Update intlMiddleware.ts

--- middlewares/intlMiddleware.ts
+++ middlewares/intlMiddleware.ts
@@ -1,14 +1,10 @@
 import { createMiddleware } from 'next-intl/middleware';
-
-// 直接在这里定义所需的配置
-const locales = ['en', 'jp', 'de', 'es', 'fr', 'pt', 'ru', 'cn', 'tw']; // 根据您的实际语言列表调整
-const defaultLocale = 'en';
-const localePrefix = 'as-needed';
+import { locales, defaultLocale } from '../i18n';
 
 const intlMiddleware = createMiddleware({
   locales,
   defaultLocale,
-  localePrefix,
+  localePrefix: 'as-needed'
 });
 
 export default intlMiddleware;
